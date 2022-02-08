d7219a066696892755c2fb2b0b2bcae4bb5d907a
n0ruSh <michael2ib1989@gmail.com>
2022-02-08 23:21:35 +0800

    feat: add back top

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import { ThemeProvider } from "next-themes";
-import { AppProps } from "next/app";
-import "../styles/index.css";
-import "@arco-design/web-react/dist/css/arco.css";
+import { ThemeProvider } from 'next-themes';
+import { AppProps } from 'next/app';
+import '../styles/index.css';
+import '@arco-design/web-react/dist/css/arco.css';
+import { BackTop } from '@arco-design/web-react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <BackTop />
     </ThemeProvider>
   );
 }

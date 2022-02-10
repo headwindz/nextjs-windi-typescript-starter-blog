45c45731130b0105099259b4ccc0a168d86055a6
n0ruSh <michael2ib1989@gmail.com>
2022-02-10 22:33:46 +0800

    feat: add twin arco

--- windi.config.ts
+++ windi.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'windicss/helpers';
+import { preset } from 'twin.arco';
 
 export default defineConfig({
   extract: {
     include: ['**/*.{jsx,tsx,css}'],
     exclude: ['node_modules', '.git', '.next/**/*'],
   },
+  presets: [preset()],
   plugins: [require('windicss/plugin/typography')],
 });

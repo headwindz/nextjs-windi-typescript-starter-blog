0a79180c0f6bd22c204348c34cec18f566a0b9c0
n0ruSh <michael2ib1989@gmail.com>
2022-03-03 23:31:48 +0800

    feat: update backtop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,27 @@
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 import '@arco-design/web-react/dist/css/arco.css';
-import { BackTop } from '@arco-design/web-react';
 import 'windi.css';
 import '../styles/index.css';
 import '../styles/prism.css';
+import { BackTop, Button } from '@arco-design/web-react';
+import { IconUp } from '@arco-design/web-react/icon';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
-      <BackTop />
+      <BackTop>
+        <Button
+          type="secondary"
+          shape="circle"
+          iconOnly
+          size="large"
+          className="right-20 bottom-8"
+        >
+          <IconUp />
+        </Button>
+      </BackTop>
     </ThemeProvider>
   );
 }
